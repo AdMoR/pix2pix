69da9df2345604366f4d3eb3f7a074892a9b011e
AdrienMorvan <adrien.morvan@photobox.com>
2018-12-05 17:28:03 +0100

    Changed the params of nets

--- run_training.py
+++ run_training.py
@@ -16,18 +16,18 @@ else:
     device = torch.device('cpu')
 
 train_transform = transforms.Compose(
-                      [transforms.Resize((224, 224)),
+                      [transforms.Resize((256, 256)),
                        #transforms.RandomResizedCrop(224),
                        transforms.RandomHorizontalFlip(),
                        torchvision.transforms.ColorJitter(0.1, 0.1, 0.1),
-                       torchvision.transforms.RandomAffine(5, [0.1, 0.1], [0.95, 1.05]),
+                       #torchvision.transforms.RandomAffine(5, [0.1, 0.1], [0.95, 1.05]),
                        transforms.ToTensor()])
 path = "/data"
 
 my_dataset = ColorizationDataset(path, transform=train_transform)
-train_data = torch.utils.data.DataLoader(my_dataset, batch_size=32, shuffle=True, num_workers=4)
-gen = UNet([3, 20, 40, 80]).to(device=device)
-disc = EncoderNet([6, 40, 80, 160, 320]).to(device=device)
+train_data = torch.utils.data.DataLoader(my_dataset, batch_size=4, shuffle=True, num_workers=2)
+gen = UNet([3, 64, 128, 256, 512, 512, 512, 512, 512]).to(device=device)
+disc = EncoderNet([6, 64, 128, 256, 512, 512, 512]).to(device=device)
 #disc = AlexNet_finetune().to(device=device)
 
 print(gen, disc)
@@ -40,23 +40,22 @@ writer = tensorboardX.SummaryWriter(log_dir="./logs", comment="pix2pix")
 for e in range(100):
     for i, (x, y) in enumerate(train_data):
         disc_optimizer.zero_grad()
-        disc_loss = adv_loss.discriminator_forward(x, y, None, verbose=i % 100 == 0)
+        disc_loss = adv_loss.discriminator_forward(x, y, None, verbose=i % 1000 == 0)
         disc_loss.backward()
         disc_optimizer.step()
 
         gen_optimizer.zero_grad()
-        gen_loss = adv_loss.generator_forward(x, y, None, verbose=i % 100 == 0)
+        gen_loss = adv_loss.generator_forward(x, y, None, verbose=i % 1000 == 0)
         gen_loss.backward()
         gen_optimizer.step()
 
         if i % 1000 == 0:
             writer.add_scalars("pix2pix/", {"Discriminator": disc_loss, "Generator loss": gen_loss}, e * len(train_data) + i)
-            print("Discriminator loss : ", disc_loss)
-            print("Generator loss : ", gen_loss)
             viz = vutils.make_grid(torch.cat([y.to(device), gen(x.to(device))], dim=0))
             viz = torch.clamp(viz, 0, 0.999999)
             writer.add_image('visu/', viz, e * len(train_data) + i)
-            pickle.dump(gen, open("generator_unet_{}.pkl".format(e), "wb"))
+
+    pickle.dump(gen, open("generator_unet_{}.pkl".format(e), "wb"))
 
 
 
